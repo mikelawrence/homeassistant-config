03b337d8b6ec01b8c915506ccabe04653e71e961
mikelawrence <mike@bigmikehome.com>
2018-06-08 13:36:17 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Home Assistant Configuration
+Here's my [Home Assistant](https://home-assistant.io/) configuration and I am using the easy to install [Hass.io](https://www.home-assistant.io/hassio/) running on a [Raspberry Pi](https://www.raspberrypi.org/products/raspberry-pi-3-model-b/).
+
+## Some of the devices and services that I use with HA
+* [ISY-994i](https://www.universal-devices.com/residential/isy994i-series/) for Insteon control.
+* Lots of SmartHome [Insteon](https://www.smarthome.com/insteon.html) devices
+* Custom MQTT devices
+  * WiFi Gate Controller for controlling my [US Automatic](https://www.usautomatic.com/) gate opener.
+    * KiCAD board design and software is available on [GitHub](https://github.com/mikelawrence/WiFi-Gate-Controller).
+  * WiFi Septic Controller for controlling and monitoring my Aerobic Septic System.
+    * KiCAD board design and software is available on [GitHub](https://github.com/mikelawrence/WiFi-Septic-Controller).
+  * Raspberry Pi RGBW LED Controller HAT for controlling a high power RGB LED Floodlight.
+    * KiCAD board design and software is available on [GitHub](https://github.com/mikelawrence/RPi-HAT-RGBW-LED-Controller).
+* Presence detection.
+  * [Unifi Controller](https://home-assistant.io/components/device_tracker.unifi/) for network device tracking.
+  * [iOS app](https://itunes.apple.com/us/app/home-assistant-companion/id1099568401?mt=8) for location tracking and notifications.
+* Climate
+  * [Nest](https://home-assistant.io/components/ecobee/) thermostats (three in total).
+  * [Davis Vantage Pro2](https://www.davisnet.com/solution/vantage-pro2/) connected to [weeWX](http://www.weewx.com/) which publishes to [Wunderground](https://www.wunderground.com/weather/us/tx/elgin/KTXELGIN7) and local MQTT server so HA can pick up current conditions.
---
+++ python_scripts/meta_device_tracker.py
@@ -0,0 +1,145 @@
+# From Github arsaboo
+# https://github.com/arsaboo/homeassistant-config
+#
+# Combine multiple device trackers into one entity
+# You can call the script using the following:
+# - service: python_script.meta_device_tracker
+#   data_template:
+#     entity_id: '{{trigger.entity_id}}'
+# OPTIONS
+# List the trackers for each individual
+MikeTrackers = ['device_tracker.mike_iphone_ios',
+                'device_tracker.mike_iphone',]
+ClaraTrackers = ['device_tracker.clara_iphone_ios',
+                 'device_tracker.clara_iphone']
+MikeyTrackers = ['device_tracker.mikey_iphone']
+BryanTrackers = ['device_tracker.bryan_iphone']
+
+# Get the entity that triggered the automation
+triggeredEntity = data.get('entity_id')
+
+# if the triggering entity is the meta device tracker then we need to
+#   use most recent tracker for tht person
+if triggeredEntity == 'device_tracker.mike_meta':
+    bestEntity = MikeTrackers[0]
+    last_changed = datetime.min
+    for entity in MikeTrackers:
+        state = hass.states.get(entity)
+        if state != None:
+            if state.last_changed > last_changed:
+                bestEntity = entity
+                last_changed = state.last_changed
+elif triggeredEntity == 'device_tracker.clara_meta':
+    bestEntity = ClaraTrackers[0]
+    last_changed = datetime.min
+    for entity in ClaraTrackers:
+        state = hass.states.get(entity)
+        if state != None:
+            if state.last_changed > last_changed:
+                bestEntity = entity
+                last_changed = state.last_changed
+elif triggeredEntity == 'device_tracker.mikey_meta':
+    bestEntity = MikeyTrackers[0]
+    last_changed = datetime.min
+    for entity in MikeyTrackers:
+        state = hass.states.get(entity)
+        if state != None:
+            if state.last_changed > last_changed:
+                bestEntity = entity
+                last_changed = state.last_changed
+elif triggeredEntity == 'device_tracker.bryan_meta':
+    bestEntity = BryanTrackers[0]
+    last_changed = datetime.min
+    for entity in BryanTrackers:
+        state = hass.states.get(entity)
+        if state != None:
+            if state.last_changed > last_changed:
+                bestEntity = entity
+                last_changed = state.last_changed
+else:
+    bestEntity = triggeredEntity
+
+# update triggeredEntity
+triggeredEntity = bestEntity
+
+# Set friendly name and the metatracker name based on the entity that triggered
+if triggeredEntity in MikeTrackers:
+    newFriendlyName = 'Mike'
+    newEntityPicture = '/local/mike.jpg'
+    metatrackerName = 'device_tracker.mike_meta'
+elif triggeredEntity in ClaraTrackers:
+    newFriendlyName = 'Clara'
+    newEntityPicture = '/local/clara.jpg'
+    metatrackerName = 'device_tracker.clara_meta'
+elif triggeredEntity in MikeyTrackers:
+    newFriendlyName = 'Mikey'
+    newEntityPicture = '/local/mikey.jpg'
+    metatrackerName = 'device_tracker.mikey_meta'
+elif triggeredEntity in BryanTrackers:
+    newFriendlyName = 'Bryan'
+    newEntityPicture = '/local/bryan.jpg'
+    metatrackerName = 'device_tracker.bryan_meta'
+else:
+    newFriendlyName = None
+    metatrackerName = None
+
+# Get current & new state
+newState = hass.states.get(triggeredEntity)
+currentState = hass.states.get(metatrackerName)
+
+# Get New data
+newSource = newState.attributes.get('source_type')
+
+# If GPS source, set new coordinates
+if newSource == 'gps':
+    newLatitude = newState.attributes.get('latitude')
+    newLongitude = newState.attributes.get('longitude')
+    newgpsAccuracy = newState.attributes.get('gps_accuracy')
+# If not, keep last known coordinates
+elif ((currentState is not None)
+      and (currentState.attributes.get('latitude') is not None)):
+    newLatitude = currentState.attributes.get('latitude')
+    newLongitude = currentState.attributes.get('longitude')
+    newgpsAccuracy = currentState.attributes.get('gps_accuracy')
+# Otherwise return null
+else:
+    newLatitude = None
+    newLongitude = None
+    newgpsAccuracy = None
+
+# Get Battery
+if newState.attributes.get('battery') is not None:
+    newBattery = newState.attributes.get('battery')
+elif ((currentState is not None)
+      and (currentState.attributes.get('battery') is not None)):
+    newBattery = currentState.attributes.get('battery')
+else:
+    newBattery = None
+
+# Get velocity
+if newState.attributes.get('velocity') is not None:
+    newVelocity = newState.attributes.get('velocity')
+elif ((currentState is not None)
+      and (currentState.attributes.get('velocity') is not None)):
+    newVelocity = currentState.attributes.get('velocity')
+else:
+    newVelocity = None
+
+# Get State
+if newState.state is not None:
+    newStatus = newState.state
+else:
+    newStatus = currentState.state
+
+# Create device_tracker.meta entity
+hass.states.set(metatrackerName, newStatus, {
+    'friendly_name': newFriendlyName,
+    'entity_picture': newEntityPicture,
+    'source_type': newSource,
+    'battery': newBattery,
+    'gps_accuracy': newgpsAccuracy,
+    'latitude': newLatitude,
+    'longitude': newLongitude,
+    'velocity': newVelocity,
+    'update_source': triggeredEntity
+})
