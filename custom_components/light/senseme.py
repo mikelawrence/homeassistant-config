eb68e3722d084895b78a289b8eb32956fa4cda4a
mikelawrence <mike@bigmikehome.com>
2018-06-28 08:38:52 -0500

    Fix light brightness errors.
    Added new functions to handle Home Assistant brightness conversions.

--- custom_components/light/senseme.py
+++ custom_components/light/senseme.py
@@ -20,6 +20,21 @@ SENSEME_LIGHT_PRESENT = 'DEVICE;LIGHT'
 
 
 
+def conv_bright_ha_to_lib(brightness) -> int:
+    """Convert HA brightness scale 0-255 to library scale 0-16."""
+    if brightness == 255:   # this will end up as 16 which is max
+        brightness = 256
+    return int(brightness / 16)
+
+
+def conv_bright_lib_to_ha(brightness) -> int:
+    """Convert library brightness scale 0-16 to HA scale 0-255."""
+    brightness = int(brightness) * 16
+    if brightness > 255:    # force big numbers into 8-bit int range
+        brightness = 255
+    return brightness
+
+
 def setup_platform(hass, config, add_devices_callback, discovery_info=None):
     """Set up the Haiku with SenseME ceiling fan light platform."""
     lights = []
@@ -55,14 +70,14 @@ class HaikuSenseMeLight(Light):
 
 
     @property
-    def brightness(self) -> str:
+    def brightness(self) -> int:
         """Return the brightness of the light."""
         try:
-            fan_brightness = int(self._hub.get_attribute(SENSEME_LIGHT_BRIGHTNESS))
-            brightness = fan_brightness * 16
+            brightness = conv_bright_lib_to_ha(
+                self._hub.get_attribute(SENSEME_LIGHT_BRIGHTNESS))
         except KeyError:
             brightness = None
-        return str(brightness)
+        return brightness
 
 
     @property
@@ -87,11 +102,11 @@ class HaikuSenseMeLight(Light):
         if brightness == None:
             # brightness undefined, use last brightness if available
             if self._last_brightness:
-                brightness = self._last_brightness
+                brightness = conv_bright_ha_to_lib(self._last_brightness)
             else:
                 brightness = 16
         else:
-            brightness = int(brightness / 16)
+            brightness = conv_bright_ha_to_lib(brightness)
         retryCount = 2
         while retryCount != 0:
             try:
