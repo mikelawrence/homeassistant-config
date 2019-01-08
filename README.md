efbb031a120cbf48e72ff62890fa7bc3579817b7
Big Mike <mikelawrence@users.noreply.github.com>
2019-01-08 10:22:56 -0600

    Massive changes to support Multiple Home Assistant instances
    Moved custom_components to their own repository.
    Added Z-Wave configuration.
    Created new custom component to support multiple instances of Home Assistant.
    Pulled out most Studio entieies and placed in new Home Assistant insstance.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Home Assistant Configuration
-Here's my [Home Assistant](https://home-assistant.io/) configuration. I am using the easy to install [Hass.io](https://www.home-assistant.io/hassio/) running on a [Raspberry Pi](https://www.raspberrypi.org/products/raspberry-pi-3-model-b/).
+# House Home Assistant Configuration
+Here's my [Home Assistant](https://home-assistant.io/) configuration for my House. I am using the easy to install [Hass.io](https://www.home-assistant.io/hassio/) running on a [Raspberry Pi](https://www.raspberrypi.org/products/raspberry-pi-3-model-b/).
 
 ## Some of the devices and services that I use with HA
 * [ISY-994i](https://www.home-assistant.io/components/isy994/) for Insteon control.
@@ -19,39 +19,6 @@ Here's my [Home Assistant](https://home-assistant.io/) configuration. I am using
     * KiCAD board design and software is available on [GitHub](https://github.com/mikelawrence/WiFi-Septic-Controller).
   * Raspberry Pi RGBW LED Controller HAT for controlling a high power RGB LED Floodlight.
     * KiCAD board design and software is available on [GitHub](https://github.com/mikelawrence/RPi-HAT-RGBW-LED-Controller).
-* Custom Haiku with SenseME fan component
-
-## Custom Haiku with SenseME fan component
-The Haiku with SenseME fan is a WiFi connected fan and installable light. This custom component uses TomFaulkner's [SenseMe](https://github.com/TomFaulkner/SenseMe) library to communicate with the fan.
-
-### Installation
-There are three senseme.py files that must be installed in the config/custom_components directory. Note the location of the three senseme.py files in their respective folders (fan/ and light/) is important. The SenseMe library will be automatically installed by Home Assistant.
-
-### Configuration
-The Haiku with SenseME fan component will automatically discover and create a fan and light (if installed) for each discovered fan. Setting ```max_number_fans``` to the number of Haiku fans on your network will speed up the discovery process but is not required. If ```include:``` is specified, discovered fans with a matching name will be added. If ```exclude``` is specified, discovered fans with a matching name will NOT be added. If both ```include:``` and ```exclude``` are specified, only ```include:``` will be honored. If neither ```include:``` and ```exclude``` are specified, all auto-detected fans will be added.
-
-For included fans you can now specify a ```friendly_name``` to use instead of ```name``` in Home Assistant. This is handy for grouped fans. Controlling any fan in a group will affect all fans of that group. Default value is the same as ```name ```. Also new in the include section is the ```has_light``` boolean which when ```true``` will add a light component along with the fan. The default for ```has_light``` is ```true```. The included fan section must have a ```name ``` variable and it must must match the name in the Haiku app.
-```yaml
-# enable Haiku with SenseMe ceiling fans
-senseme:
-  max_number_fans: 2
-  # used to include only specific fans
-  include:
-    - name: "Studio Vault Fan"
-      friendly_name: "Studio Fan"
-      has_light: true
-    - name: "Family Room Fan"
-  # or use exclude to prevent specific auto-detected fan
-  exclude:
-    - "Studio Beam Fan"
-senseme:
-  max_number_fans: 2
-  # used to include specific fan
-  include:
-    - "Studio Vault Fan"
-    - "Studio Beam Fan"
-```
-
-### Problems
-* Occasionally changes to the fan state fail to connect to fan and make the change, usually as a network (python socket) error. Same thing is true for the SenseMe background task which gets the complete fan state every minute.
-* Originally the Senseme custom component auto-detected both the existence of a light and the fan's group but longer term usage showed a problem with consistently auto-detecting these values. This version no longer auto-detects these values and requires the user to specify them in advance.
+* Custom components (see [homeassistant-custom-components](https://github.com/mikelawrence/homeassistant-custom-components) repository)
+  * Haiku with SenseME fan and light component
+  * MQTT_sharing component for multiple Home Assistant instances
--- python_scripts/meta_device_tracker.py
+++ python_scripts/meta_device_tracker.py
@@ -7,139 +7,142 @@
 #   data_template:
 #     entity_id: '{{trigger.entity_id}}'
 # OPTIONS
-# List the trackers for each individual
-MikeTrackers = ['device_tracker.mike_iphone_ios',
-                'device_tracker.mike_iphone',]
-ClaraTrackers = ['device_tracker.clara_iphone_ios',
-                 'device_tracker.clara_iphone']
-MikeyTrackers = ['device_tracker.mikey_iphone']
-BryanTrackers = ['device_tracker.bryan_iphone']
+
+people = {
+    'device_tracker.mike_meta' : {
+        'friendly_name' : 'Mike',
+        'entity_picture': '/local/mike.jpg',
+        'trackers'      : [
+            'device_tracker.mike_iphone_ios',
+            'device_tracker.mike_iphone'
+        ]
+    },
+    'device_tracker.clara_meta' : {
+        'friendly_name' : 'Clara',
+        'entity_picture': '/local/clara.jpg',
+        'trackers'      : [
+            'device_tracker.clara_iphone_ios',
+            'device_tracker.clara_iphone'
+        ]
+    },
+    'device_tracker.mikey_meta' : {
+        'friendly_name' : 'Mikey',
+        'entity_picture': '/local/mikey.jpg',
+        'trackers'      : [
+            'device_tracker.mikey_iphone'
+        ]
+    },
+    'device_tracker.bryan_meta' : {
+        'friendly_name' : 'Bryan',
+        'entity_picture': '/local/bryan.jpg',
+        'trackers'      : [
+            'device_tracker.bryan_iphone'
+        ]
+    },
+    'device_tracker.jim_meta' : {
+        'friendly_name' : 'Jim',
+        'entity_picture': '/local/jim.jpg',
+        'trackers'      : [
+            'device_tracker.jim_phone'
+        ]
+    },
+    'device_tracker.kasey_meta' : {
+        'friendly_name' : 'Kasey',
+        'entity_picture': '/local/kasey.jpg',
+        'trackers'      : [
+            'device_tracker.kasey_phone'
+        ]
+    }
+}
 
 # Get the entity that triggered the automation
 triggeredEntity = data.get('entity_id')
 
 # if the triggering entity is the meta device tracker then we need to
 #   use most recent tracker for that person
-if triggeredEntity == 'device_tracker.mike_meta':
-    bestEntity = MikeTrackers[0]
-    last_changed = hass.states.get(bestEntity).last_changed
-    for entity in MikeTrackers:
+# the only time this happens is when the start script calls this script
+if triggeredEntity in people:
+    person = people.get(triggeredEntity)
+    triggeredEntity = person.get('trackers')[0]
+    last_changed = hass.states.get(triggeredEntity).last_changed
+    for entity in person.get('trackers'):
         state = hass.states.get(entity)
         if state != None:
             if state.last_changed > last_changed:
-                bestEntity = entity
+                triggeredEntity = entity
                 last_changed = state.last_changed
-elif triggeredEntity == 'device_tracker.clara_meta':
-    bestEntity = ClaraTrackers[0]
-    last_changed = hass.states.get(bestEntity).last_changed
-    for entity in ClaraTrackers:
-        state = hass.states.get(entity)
-        if state != None:
-            if state.last_changed > last_changed:
-                bestEntity = entity
-                last_changed = state.last_changed
-elif triggeredEntity == 'device_tracker.mikey_meta':
-    bestEntity = MikeyTrackers[0]
-    last_changed = hass.states.get(bestEntity).last_changed
-    for entity in MikeyTrackers:
-        state = hass.states.get(entity)
-        if state != None:
-            if state.last_changed > last_changed:
-                bestEntity = entity
-                last_changed = state.last_changed
-elif triggeredEntity == 'device_tracker.bryan_meta':
-    bestEntity = BryanTrackers[0]
-    last_changed = hass.states.get(bestEntity).last_changed
-    for entity in BryanTrackers:
-        state = hass.states.get(entity)
-        if state != None:
-            if state.last_changed > last_changed:
-                bestEntity = entity
-                last_changed = state.last_changed
-else:
-    bestEntity = triggeredEntity
 
-# update triggeredEntity
-triggeredEntity = bestEntity
+# default is no matching meta entity
+metaEntity = None
+# find the person that matches the triggeredEntity
+for key,val in people.items():
+    person = people.get(key)
+    if triggeredEntity in person.get('trackers'):
+        # we have a match
+        metaEntity = key
+        newFriendlyName = person.get('friendly_name')
+        newEntityPicture = person.get('entity_picture')
 
-# Set friendly name and the metatracker name based on the entity that triggered
-if triggeredEntity in MikeTrackers:
-    newFriendlyName = 'Mike'
-    newEntityPicture = '/local/mike.jpg'
-    metatrackerName = 'device_tracker.mike_meta'
-elif triggeredEntity in ClaraTrackers:
-    newFriendlyName = 'Clara'
-    newEntityPicture = '/local/clara.jpg'
-    metatrackerName = 'device_tracker.clara_meta'
-elif triggeredEntity in MikeyTrackers:
-    newFriendlyName = 'Mikey'
-    newEntityPicture = '/local/mikey.jpg'
-    metatrackerName = 'device_tracker.mikey_meta'
-elif triggeredEntity in BryanTrackers:
-    newFriendlyName = 'Bryan'
-    newEntityPicture = '/local/bryan.jpg'
-    metatrackerName = 'device_tracker.bryan_meta'
-else:
-    newFriendlyName = None
-    metatrackerName = None
 
-# Get current & new state
-newState = hass.states.get(triggeredEntity)
-currentState = hass.states.get(metatrackerName)
+# do nothing if triggeredEntity is not a tracker for any of our people
+if metaEntity != None:
+    # Get current & new state
+    newState = hass.states.get(triggeredEntity)
+    currentState = hass.states.get(metaEntity)
 
-# Get New data
-newSource = newState.attributes.get('source_type')
+    # Get New data
+    newSource = newState.attributes.get('source_type')
 
-# If GPS source, set new coordinates
-if newSource == 'gps':
-    newLatitude = newState.attributes.get('latitude')
-    newLongitude = newState.attributes.get('longitude')
-    newgpsAccuracy = newState.attributes.get('gps_accuracy')
-# If not, keep last known coordinates
-elif ((currentState is not None)
-      and (currentState.attributes.get('latitude') is not None)):
-    newLatitude = currentState.attributes.get('latitude')
-    newLongitude = currentState.attributes.get('longitude')
-    newgpsAccuracy = currentState.attributes.get('gps_accuracy')
-# Otherwise return null
-else:
-    newLatitude = None
-    newLongitude = None
-    newgpsAccuracy = None
+    # If GPS source, set new coordinates
+    if newSource == 'gps':
+        newLatitude = newState.attributes.get('latitude')
+        newLongitude = newState.attributes.get('longitude')
+        newgpsAccuracy = newState.attributes.get('gps_accuracy')
+    # If not, keep last known coordinates
+    elif ((currentState is not None)
+          and (currentState.attributes.get('latitude') is not None)):
+        newLatitude = currentState.attributes.get('latitude')
+        newLongitude = currentState.attributes.get('longitude')
+        newgpsAccuracy = currentState.attributes.get('gps_accuracy')
+    # Otherwise return null
+    else:
+        newLatitude = None
+        newLongitude = None
+        newgpsAccuracy = None
 
-# Get Battery
-if newState.attributes.get('battery') is not None:
-    newBattery = newState.attributes.get('battery')
-elif ((currentState is not None)
-      and (currentState.attributes.get('battery') is not None)):
-    newBattery = currentState.attributes.get('battery')
-else:
-    newBattery = None
+    # Get Battery
+    if newState.attributes.get('battery') is not None:
+        newBattery = newState.attributes.get('battery')
+    elif ((currentState is not None)
+          and (currentState.attributes.get('battery') is not None)):
+        newBattery = currentState.attributes.get('battery')
+    else:
+        newBattery = None
 
-# Get velocity
-if newState.attributes.get('velocity') is not None:
-    newVelocity = newState.attributes.get('velocity')
-elif ((currentState is not None)
-      and (currentState.attributes.get('velocity') is not None)):
-    newVelocity = currentState.attributes.get('velocity')
-else:
-    newVelocity = None
+    # Get velocity
+    if newState.attributes.get('velocity') is not None:
+        newVelocity = newState.attributes.get('velocity')
+    elif ((currentState is not None)
+          and (currentState.attributes.get('velocity') is not None)):
+        newVelocity = currentState.attributes.get('velocity')
+    else:
+        newVelocity = None
 
-# Get State
-if newState.state is not None:
-    newStatus = newState.state
-else:
-    newStatus = currentState.state
+    # Get State
+    if newState.state is not None:
+        nextState = newState.state
+    else:
+        nextState = currentState.state
 
-# Create device_tracker.meta entity
-hass.states.set(metatrackerName, newStatus, {
-    'friendly_name': newFriendlyName,
-    'entity_picture': newEntityPicture,
-    'source_type': newSource,
-    'battery': newBattery,
-    'gps_accuracy': newgpsAccuracy,
-    'latitude': newLatitude,
-    'longitude': newLongitude,
-    'velocity': newVelocity,
-    'update_source': triggeredEntity
-})
+    # Create device_tracker.meta entity
+    hass.states.set(metaEntity, nextState, {
+        'friendly_name': newFriendlyName,
+        'entity_picture': newEntityPicture,
+        'source_type': newSource,
+        'battery': newBattery,
+        'gps_accuracy': newgpsAccuracy,
+        'latitude': newLatitude,
+        'longitude': newLongitude,
+        'velocity': newVelocity,
+        'update_source': triggeredEntity
+    })
